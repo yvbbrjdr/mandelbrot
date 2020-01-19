33b6183ed4cafdd4ee68fbc829aaee197ea6bd6b
yvbbrjdr <xyyqzy@hotmail.com>
2020-01-19 08:32:14 -0800

    Reduce number of goroutines

--- pkg/mandelbrot/mandelbrot.go
+++ pkg/mandelbrot/mandelbrot.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/big"
 	"os"
+	"runtime"
 )
 
 func Mandelbrot(p *Complex, maxIter int, julia *Complex) int {
@@ -25,11 +26,25 @@ func Mandelbrot(p *Complex, maxIter int, julia *Complex) int {
 	return -1
 }
 
+func Row(xMin, xSpan, y *big.Float, width, maxIter int, julia *Complex) []int {
+	fWidth := big.NewFloat(float64(width))
+
+	ret := make([]int, width)
+	for j := 0; j < width; j++ {
+		x := big.NewFloat(float64(j))
+		x.Mul(x, xSpan).Quo(x, fWidth).Add(x, xMin)
+
+		ret[j] = Mandelbrot(NewComplex(x, y), maxIter, julia)
+	}
+
+	return ret
+}
+
 func Plane(xMin, yMin, xSpan *big.Float, width, height, maxIter int,
 	julia *Complex) []int {
 	type dataPacket struct {
-		index int
-		data  int
+		offset int
+		data   []int
 	}
 
 	fWidth := big.NewFloat(float64(width))
@@ -37,32 +52,31 @@ func Plane(xMin, yMin, xSpan *big.Float, width, height, maxIter int,
 	ySpan := big.NewFloat(0).Mul(xSpan, fHeight)
 	ySpan.Quo(ySpan, fWidth)
 
-	ret := make([]int, width*height)
+	dataChan := make(chan dataPacket, height)
+	signalChan := make(chan struct{}, runtime.NumCPU())
 	for i := 0; i < height; i++ {
 		fmt.Fprintf(os.Stderr, "Computing Row %d/%d\r", i+1, height)
 
 		y := big.NewFloat(float64(height - i - 1))
 		y.Mul(y, ySpan).Quo(y, fHeight).Add(y, yMin)
 
-		c := make(chan dataPacket, width)
-		for j := 0; j < width; j++ {
-			x := big.NewFloat(float64(j))
-			x.Mul(x, xSpan).Quo(x, fWidth).Add(x, xMin)
-			index := i*width + j
-
-			go func() {
-				c <- dataPacket{
-					index,
-					Mandelbrot(NewComplex(x, y), maxIter, julia),
-				}
-			}()
-		}
-		for j := 0; j < width; j++ {
-			packet := <-c
-			ret[packet.index] = packet.data
-		}
+		offset := i * width
+		signalChan <- struct{}{}
+		go func() {
+			dataChan <- dataPacket{
+				offset,
+				Row(xMin, xSpan, y, width, maxIter, julia),
+			}
+			<-signalChan
+		}()
 	}
 	fmt.Fprintln(os.Stderr)
+
+	ret := make([]int, width*height)
+	for i := 0; i < height; i++ {
+		data := <-dataChan
+		copy(ret[data.offset:], data.data)
+	}
 
 	var m int
 	for k, v := range ret {
